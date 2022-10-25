768278a44ca2a196b196655f97a4f38997ceb78e
Adri <115579621+AdriMG10@users.noreply.github.com>
2022-10-25 23:51:06 +0200

    Add files via upload

---
+++ HolaMundo.java
@@ -0,0 +1,10 @@
+/**
+ *  Adri
+ */
+
+public class HolaMundo {                            //Declaro la clase con el mismo nombre del archivo 
+    public static void main(String[] args) {        //Declaro la función main de la clase en la que se ejecutará el codigo
+
+        System.out.println("Hola Mundo");           //Imprime Hola Mundo y hace un salto de linea 
+    }
+}
---
+++ ParImpar.java
@@ -0,0 +1,26 @@
+/**
+ *  Adri
+ */
+
+import java.util.Scanner;
+
+public class ParImpar {
+    public static void main (String[] args) {
+
+        long num;
+        String introduce = "Introduce un entero:";
+        String par = "El entero introducido es par";
+        String impar = "El entero introducido es impar";
+
+        Scanner leer = new Scanner (System.in);
+
+        System.out.println (introduce);
+        num = leer.nextLong();
+
+        if (num % 2 == 0) 
+            System.out.println (par);
+
+        else 
+            System.out.println (impar);           
+    }
+}
