6fdd5ce02e8b3f0fbab66e2b60cc7cd47053f467
Adri <amergam1012@g.educaand.es>
2022-11-09 09:46:16 +0100

    1.0

--- HolaMundo.java
+++ HolaMundo.java
@@ -5,7 +5,7 @@
 public class HolaMundo {                            //Declaro la clase con el mismo nombre del archivo 
     public static void main(String[] args) {        //Declaro la función main de la clase en la que se ejecutará el codigo
 
-        System.out.println("Hola Mundo");           //Imprime Hola Mundo y hace un salto de linea 
+        System.out.println("Hola Mundo");        //Imprime Hola Mundo y hace un salto de linea 
     }
 }
 
