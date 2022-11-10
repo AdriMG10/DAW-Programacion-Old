2cc1bceffbdeeed339837d1b118bf4e606cf8074
Adri <amergam1012@g.educaand.es>
2022-11-10 13:31:13 +0100

    Create EjemploFactorial.java

---
+++ EjemploFactorial.java
@@ -0,0 +1,12 @@
+public class EjemploFactorial {
+    public static void main(String[] args) {
+        
+        final int num = 3;
+        int resultado = 1;
+
+        for (int i = num; i > 0; i--) 
+            resultado *= i;
+        
+        System.out.println(num + "! es igual a: " + resultado);     
+    }
+}
