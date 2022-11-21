4fa750aa0ab1773f61cb481d5cc1d501b115c401
Adri <amergam1012@g.educaand.es>
2022-11-21 10:51:06 +0100

    1.0

--- AnioBisiesto.java
+++ AnioBisiesto.java
@@ -1,32 +1,25 @@
-/**
- *  Adri
- */
-
-import java.util.Scanner;
-
 public class AnioBisiesto {
+    /**
+     * <p>Calcula si un año dado a través de una variable es un año bisiesto o no, mostrando este resultado en la consola.</p>
+     * 
+     * @author Adri
+     * @version 1.0 
+     * @param args main  
+     * 
+     */
 
     public static void main (String[] args) {
 
-        int anio;
-        String introduce = "Introduce un año:";
-        String bisiesto = "El año introducido es bisiesto";
-        String nobisiesto = "El año introducido no es bisiesto";
-
-        Scanner leer = new Scanner (System.in);
-
-        System.out.println (introduce);
-        anio = leer.nextInt();
-
-        if (anio % 400 == 0) 
-            System.out.println (bisiesto);
+        int anio = 2022;                                                            //Creo la variable año y le asigno un año
+        
+        if (anio % 400 == 0)                                                        //Si el año es divisible entre 400:
+            System.out.println ("El año " + anio + " es bisiesto");                 //Imprime que es bisiesto
 
         else 
-            if ((anio % 4 == 0) && (anio % 100 != 0))
-                System.out.println (bisiesto);
+            if ((anio % 4 == 0) && (anio % 100 != 0))                               //Si el año es divisible entre 4 y no divisible entre 100:
+                System.out.println ("El año " + anio + " es bisiesto");             //Imprime que es bisiesto
 
             else
-                System.out.println (nobisiesto);   
-            
+                System.out.println ("El año " + anio + " no es bisiesto");          //Si no, imprime que no es bisiesto    
     }
 }
